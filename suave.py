ad1646d4318cbaf925e0a5ea60c3435a10f8d732
jsmorales27 <37352283+jsmorales27@users.noreply.github.com>
2018-04-14 20:50:40 -0700

    SIrve

--- suave.py
+++ suave.py
@@ -9,24 +9,32 @@ def gaussiana(a,sigma,t):
 	curva=curva/np.trapz(curva)
 	return curva
 
-def fourier(y):
-	N=float(len(y))
-	G=[]
-	for n in range (0, len(y)):
-		suma=[]
-		n1=float(n)
-		for k in range (0, len(y)-1):
-			distribucion=np.cos(2*math.pi*k*n1/N)-1j*np.sin(2*math.pi*k*n1/N)
-			suma.append(y[k]*distribucion)
-		G.append(np.sum(np.array(suma)))
-	return np.array(G)
+def fourier(matriz, tamano):
+	N=float(matriz.shape[1])
+	M=float(matriz.shape[0])
+	G1=np.zeros(tamano)
+	G = G1.astype(complex)
+	for u in range (0, matriz.shape[0]):
+		for v in range (0,matriz.shape[1]):
+			sumax=[]
+			for x in range (0, matriz.shape[0]-1):
+				distribucionx=np.cos(2*math.pi*x*u/M)-1j*np.sin(2*math.pi*x*u/M)
+				sumay=[]
+				for y in range (0, matriz.shape[1]-1):
+					distribuciony=np.cos(2*math.pi*y*v/N)-1j*np.sin(2*math.pi*y*v/N)
+					sumay.append(matriz[x,y]*distribuciony)
+				sumay_total=np.sum(np.array(sumay))
+				sumax.append(sumay_total*distribucionx)
+			sumax_total=np.sum(np.array(sumax))
+			G[u,v]=sumax_total
+	return G
 
-img = plt.imread('totti.png')
+img = plt.imread('cristiano.png')
 print np.shape(img)
 
 from scipy import fftpack
 # Hacer la funcion gaussiana con una parametrizacion t
-t = np.linspace(-10, 10, 30)
+t = np.linspace(-10, 10, 256)
 gauss1d = gaussiana(1.0,np.sqrt(10.0),t)
 
 #curva gaussiana de dos dimensiones a partir de la multiplicacion de vectores gaussianos
@@ -35,8 +43,14 @@ print gauss1d[:, np.newaxis].shape
 print gauss1d[np.newaxis, :].shape
 print(gauss2d.shape)
 
-# padded fourier transform, with the same shape as the image
+# hacer la transformada de fourier bidimensional de la matriz gaussiana, con la dim de salida igual a dim img 
 kernel_ft = fftpack.fft2(gauss2d, shape=img.shape[:2], axes=(0, 1))
+gauss2d_transformada=fourier(gauss2d, np.shape(gauss2d))
+
+plt.imshow(   abs(fftpack.fftshift(gauss2d_transformada)))
+plt.savefig("fotogauss_trans.png")
+plt.clf()
+
 
 # convolve
 img_ft = fftpack.fft2(img, axes=(0, 1))
@@ -44,7 +58,7 @@ img2_ft = kernel_ft[:, :, np.newaxis] * img_ft
 img2 = fftpack.ifft2(img2_ft, axes=(0, 1)).real
 
 plt.imshow(img2)
-plt.savefig("capitano2.png")
+plt.savefig("capitano3.png")
 
 #visualizar gaussiana dos dimensional
 from mpl_toolkits.mplot3d import Axes3D
